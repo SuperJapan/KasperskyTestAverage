fc71094fc6098869409718c3e6e5b09970e2f70c
TestCat <serverov2014@yandex.ru>
2015-11-23 21:15:46 +0400

    Upd

---
+++ UnitTestProject1/TestClass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject1
+{
+    class TestClass
+    {
+        public TestClass()
+        {
+            
+        }
+        
+        public static string ExceptionNullArgument = "ExceptionNullArgument";
+        public static string ExceptionOutOfRangeArgument = "ExceptionNullArgument";
+
+        public double Average(IEnumerable<double> sequence)
+        {
+            if (sequence == null)
+            {
+                throw new ArgumentNullException("sequence", ExceptionNullArgument);
+            }
+            if (sequence.Count() < 1)
+            {
+                throw new ArgumentOutOfRangeException("sequence", ExceptionOutOfRangeArgument);
+            }
+
+            double res = 0;
+            double sum = 0;
+            foreach (double d in sequence)
+                sum += d;
+            
+            res = sum / (double)sequence.Count();
+            return res;
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestAverage()
+        {
+            double expected = 0.3;
+            
+            IEnumerable<double> doubleEnum = new List<double>() { 0.1, 0.2, 0.3, 0.4, 0.5 };                        
+            TestClass cl = new TestClass();
+            double res = cl.Average(doubleEnum);
+            
+            Assert.AreEqual(expected, res, 0.001, "Not expected value");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TestAverageArgumentNullException()
+        {
+            TestClass cl = new TestClass();
+            double res = cl.Average(null);
+
+            // assert is handled by ExpectedException
+        }
+        
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestAverageArgumentOutOfRangeException()
+        {
+            IEnumerable<double> doubleEnum = new List<double>() { };
+            TestClass cl = new TestClass();
+            double res = cl.Average(doubleEnum);
+            
+            // assert is handled by ExpectedException
+        }
+    }
+}
